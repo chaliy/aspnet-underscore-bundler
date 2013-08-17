8f1d7a081be75768bc567e56f7a16ca2f388da48
Mike Chaliy <mike@chaliy.name>
2013-08-17 22:19:21 +0300

    Small cleanup to project

--- src/UnderscoreBundler/CompiledUndrescoreTemplatesBundler.cs
+++ src/UnderscoreBundler/CompiledUndrescoreTemplatesBundler.cs
@@ -14,25 +14,11 @@ namespace UnderscoreBundler
             {
                 return string.Empty;
             }
-            //if (context == null)
-            //{
-            //    throw new ArgumentNullException("context");
-            //}
-
+            
             var buffer = new StringBuilder();
-            //var id = "";
-            //if (context.EnableInstrumentation)
-            //{
-            //    id = DefaultBundleBuilder.GetBoundaryIdentifier(bundle);
-            //    stringBuilder.AppendLine(DefaultBundleBuilder.GenerateBundlePreamble(id));
-            //}
+            
             foreach (var current in files)
             {
-                //if (context.EnableInstrumentation)
-                //{
-                //    stringBuilder.Append(DefaultBundleBuilder.GetFileHeader(appPath, current, DefaultBundleBuilder.GetInstrumentedFileHeaderFormat(id)));
-                //}
-
                 if (current.Extension.Equals(".html", StringComparison.OrdinalIgnoreCase))
                 {
                     var compiler = new UnderscoreTemplateCompiler();
